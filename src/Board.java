ca6ccf097ad7db7dabe17899d5102b4586e3e8b8
chaserylle <oneandonlychasey@gmail.com>
2018-12-11 23:13:55 +0800

    set initial state

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,20 @@
 
+/**
+ * @author Chasey
+ *
+ */
+/**
+ * @author Chasey
+ *
+ */
+/**
+ * @author Chasey
+ *
+ */
+/**
+ * @author Chasey
+ *
+ */
 public class Board {
 
 	Cell[][] board = new Cell[9][8];
@@ -25,16 +41,27 @@ public class Board {
 		
 	}
 	
+	/**
+	 * prints the board
+	 */
 	public void printBoard() {
 		int i;
 		int j;
 		int k;
 		
-		for(i=7; i>=0; i--) {
+		//had to separately print first row because it does not align.
+		System.out.print(board[0][6].getOccupied() + "           ");
+		System.out.print(board[2][6].getOccupied() + "           ");
+		System.out.print(board[4][6].getOccupied() + "           ");
+		System.out.print(board[6][6].getOccupied() + "           ");
+		System.out.print(board[8][6].getOccupied() + "           ");
+		System.out.println();
+		
+		for(i=5; i>=0; i--) {
 			for(j=0; j<9; j++) {
 				if(board[j][i]!=null)
 					if(j%2!=0) {
-						System.out.print(board[j][i].getRow() +":" + board[j][i].getColumn() + "    ");
+						System.out.print(board[j][i].getOccupied() + "    ");
 					}
 				else
 					System.out.print("       ");
@@ -43,12 +70,28 @@ public class Board {
 			for(j=0; j<9; j++) {
 				if(board[j][i]!=null)
 					if(j%2==0) {
-						System.out.print(board[j][i].getRow() +":" + board[j][i].getColumn() + "    ");
+						System.out.print(board[j][i].getOccupied() + "    ");
 					}
 				else
 					System.out.print("       ");
 			}
 			System.out.println();
 		}
+	}
+	
+	
+	/**
+	 * @param column
+	 * @param row
+	 * @return valid if the initial state is successfully set and is valid.
+	 * 
+	 * sets the initial state of the board
+	 * 6 chips around (column,row) with alternating chips
+	 * top, upper right, lower right, bottom, lower left and upper left.
+	 */
+	public boolean setInitialState(int column, int row) {
+		boolean valid = true;
+		
+		return valid;
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -6,22 +6,53 @@
  * @author Chasey
  *
  */
+
+import java.util.Scanner;
 public class Game {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Scanner kbd = new Scanner(System.in);
+		int choice;
 		Board X = new Board();
-		X.generateBoard();
-	}
-	
-	public void checkAbove(int column, int row) {
-		if(row>0 && row<6) {
-			for(int i=row; i<6; i++) {
+		
+		boolean loop = true;
+		while(loop) {
+			System.out.println("----------------");
+			System.out.println("PROGRAM OPTIONS");
+			System.out.println("----------------");
+			System.out.println("1. Set initial state.");
+			System.out.println("2. Print board.");
+			System.out.println("0. Exit");
+			System.out.print("Enter Choice: ");
+			choice = kbd.nextInt();
+			
+			switch(choice){
+			
+			case(1): //set initial state
+				int centerColumn;
+				int centerRow;
+				System.out.println("Enter the center of the initial tablue.");
+				System.out.print("Enter the column number of the vertex: ");
+				centerColumn = kbd.nextInt();
+				System.out.print("Enter the row number of the vertex: ");
+				centerRow = kbd.nextInt();
+				break;
 				
+			case(2):
+				X.generateBoard();
+				break;
+				
+			case(0):
+				System.out.println("Goodbye!");
+				loop = false;
+				break;
 			}
+			System.out.println();
 		}
 	}
+	
 
 }
